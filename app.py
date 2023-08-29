d1bf8688d884d2e5e23557a5a2fc394bbc59a669
BilboBaGG <xxxeromant@gmail.com>
2023-08-29 15:08:15 -0400

    Arparse initialized; Web password configured

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 from flask_cors import CORS
 import yaml 
+import argparse
+
+parser = argparse.ArgumentParser(description='Web inteface for the most popular penetration testing tools.', epilog="AutoPenetration by CyberFloppa")
+parser.add_argument('-p', dest='password', help='pass for admin user')
 
 fileconfig = open("config.yaml")
 configuration = yaml.load(fileconfig, Loader=yaml.FullLoader)
@@ -16,9 +20,20 @@ CORS(app)
 def ping_pong():
     return "pong!"
 
+@app.route('/', methods=['GET'])
+def index():
+    return app.config["admin_password"]
+
+
 @app.route('/login', methods=['GET'])
 def login():
     return render_template("login.html")
 
 if __name__ == '__main__':
-    app.run(host=flask_conf["host"], port=flask_conf["port"], debug=flask_conf["debug"])
+    args = parser.parse_args()
+
+    if args.password:
+        app.config["admin_password"] = args.password
+        app.run(host=flask_conf["host"], port=flask_conf["port"], debug=flask_conf["debug"])
+    else:
+        parser.print_help()
